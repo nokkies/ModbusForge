9b2cdfcbe1ce52787ffdb4b8e092b2fdf402f887
Reinach van Nieuwenhuizen <skordonkels@gmail.com>
2025-08-19 12:51:29 +1000

    Add project files.

---
+++ ModbusForge/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ModbusForge
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
