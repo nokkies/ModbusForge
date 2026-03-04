02f87b39dd5a9c94363ab7d54fce2be4bb724ced
nokkies <skordonkels@gmail.com>
2026-03-04 12:54:24 +1000

    Merge and update version to 2.8.0 with trend sampling optimizations and test fixes

--- ModbusForge/App.xaml.cs
+++ ModbusForge/App.xaml.cs
@@ -81,7 +81,12 @@ namespace ModbusForge
             services.AddSingleton<IScriptRuleService, ScriptRuleService>();
             
             // Register Coordinators
-            services.AddSingleton<ConnectionCoordinator>();
+            services.AddSingleton<ConnectionCoordinator>(provider => new ConnectionCoordinator(
+                provider.GetRequiredService<ModbusTcpService>(),
+                provider.GetRequiredService<ModbusServerService>(),
+                provider.GetRequiredService<IConsoleLoggerService>(),
+                provider.GetRequiredService<ILogger<ConnectionCoordinator>>()
+            ));
             services.AddSingleton<RegisterCoordinator>();
             services.AddSingleton<CustomEntryCoordinator>();
             services.AddSingleton<TrendCoordinator>(provider => new TrendCoordinator(
